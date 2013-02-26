65028527347aa292242c30b3d6dc5aa182ebc54f
Adam Bronte <abronte@gmail.com>
2013-02-26 12:36:19 -0800

    blank rails 4 app with ruby 2.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LunchTwits::Application.config.secret_key_base = 'dbaf7b09488fde6dd1e590d002d5936c66c5b7df8875cb0578ee15779cf461b086eae89ba4bdcdd5ba4f1a3e3f5f2607e1eb7b4253b0a714d34ece58b00992e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LunchTwits::Application.config.session_store :encrypted_cookie_store, key: '_LunchTwits_session'
