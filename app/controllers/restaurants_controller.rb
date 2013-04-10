75951f52a2233a5e15a2ff6517526e008c21ed16
Ian Young <ian.greenleaf@gmail.com>
2013-04-10 12:06:51 -0700

    Restaurants controller

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,19 @@
+class RestaurantsController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130227193848) do
+ActiveRecord::Schema.define(version: 20130410185654) do
+
+  create_table "restaurants", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "address"
+    t.string   "menu_url"
+    t.string   "ordering_url"
+    t.string   "phone_number"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "provider"
