45ff776a4777339c5aeecb1a040970918c9039aa
Ian Young <ian.greenleaf@gmail.com>
2013-04-10 12:15:58 -0700

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Restaurant.create!({
+  name: "Wich Wich",
+  description: "Sub sandwiches",
+  address: "926 Orange Ave, Coronado CA",
+  menu_url: "http://www.whichwich.com/menuV5.5",
+  ordering_url: "http://www.whichwich.com/online_ordering",
+  phone_number: "619-522-9424",
+})
