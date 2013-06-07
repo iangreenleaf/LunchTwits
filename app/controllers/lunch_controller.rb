9a80b1686726703cdde5fac789044254d24521a3
Ian Young <ian.greenleaf@gmail.com>
2013-06-06 17:07:03 -0700

    More views and actions and seeds

--- app/controllers/lunch_controller.rb
+++ app/controllers/lunch_controller.rb
@@ -1,5 +1,13 @@
 class LunchController < ApplicationController
   def index
+    @lunches = Lunch.all
+  end
+
+  def show
+    @lunch = Lunch.find params[:id]
+  end
+
+  def new
   end
 
   def edit
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,21 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-Restaurant.create!({
+restaurant = Restaurant.create!({
   name: "Wich Wich",
   description: "Sub sandwiches",
   address: "926 Orange Ave, Coronado CA",
   menu_url: "http://www.whichwich.com/menuV5.5",
   ordering_url: "http://www.whichwich.com/online_ordering",
   phone_number: "619-522-9424",
+})
+
+user = User.create!({
+  username: "test",
+  name: "Test user",
+})
+
+lunch = Lunch.create!({
+  title: "Yummy Tuesday lunch",
+  restaurant: restaurant,
 })
